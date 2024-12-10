882ec3163f24ed4f48dfaf99e2dd783157dc5ee0
DNSO0 <toxicthrashd@gmail.com>
2024-12-10 18:41:49 -0500

    Configurar rutas básicas con React Router DOM

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './assets/css/componentes/card.css'
 import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <>
-      <Home />
-      <Sobre />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/sobre' element={<Sobre/>} />
+      </Routes>
+    </Router>
   );
 }
 
